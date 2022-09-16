516213d3cbeb7c61279a5c48631adc70e9a844ab
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2022-09-16 14:01:39 +0800

    add install script

---
+++ install/install.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+INSTALL_FILE="./aur.txt"
+AUR_HELPER="yay -S --noconfirm --needed"
+# AUR_HELPER="echo"
+
+sed -e 's/#.*//; /^\s*$/d;' $INSTALL_FILE | while read -r line
+# s/#.*// remove text after #
+# s/^ *// left trim
+# s/ *$// right trim
+# /^$/d delete empty line
+# ! unintended behaviour: the last line will not be printed
+# ! so add extra space in the end
+do
+    $AUR_HELPER "${line}"
+done
+
