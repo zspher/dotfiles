3ff20e6c22ffe15cbb88d665ab548139c2973a5e
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2024-01-14 22:25:40 +0800

    feat(nvim): just use from git

--- configs/neovim/default.nix
+++ configs/neovim/default.nix
@@ -2,15 +2,7 @@
   pkgs,
   config,
   ...
-}: let
-  neovim_src = ./nvim;
-in {
-  xdg.configFile."nvim/lua".source = neovim_src + "/lua";
-  xdg.configFile."nvim/spell".source = neovim_src + "/spell";
-  xdg.configFile."nvim/ftplugin".source = neovim_src + "/ftplugin";
-  xdg.configFile."nvim/snippets".source = neovim_src + "/snippets";
-  xdg.configFile."nvim/init.lua".source = neovim_src + "/init.lua";
-
+}: {
   programs = {
     neovim = {
       enable = true;
