36a9bdb6f29a9da7cfb74b1de4c8477e50d33170
portus-zephyr <66728045+portus-zephyr@users.noreply.github.com>
2024-01-14 23:13:09 +0800

    feat: pi add root

--- system/pi.nix
+++ system/pi.nix
@@ -1,4 +1,8 @@
 {pkgs, ...}: {
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
+    fsType = "ext4";
+  };
   nixpkgs.hostPlatform = "aarch64-linux";
   boot = {
     kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
