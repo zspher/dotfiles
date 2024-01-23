b17f7a986ea9fdf827834c970a6b876fd7ba6b44
zspher <66728045+zspher@users.noreply.github.com>
2024-01-23 19:29:07 +0800

    feat: add audio

--- system/base.nix
+++ system/base.nix
@@ -65,4 +65,14 @@ in {
       openDefaultPorts = true;
     };
   };
+
+  # audio
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
 }
