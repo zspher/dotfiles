90cbf89698dbe82d29b235c2139dd91fc9c03140
zspher <66728045+zspher@users.noreply.github.com>
2024-01-25 08:59:23 +0000

    feat: add xdg dir

--- configs/home-manager.nix
+++ configs/home-manager.nix
@@ -1,9 +1,19 @@
-{data, ...}: let
+{
+  config,
+  data,
+  ...
+}: let
   inherit (data) username;
 in {
   home = {
     inherit username;
     homeDirectory = "/home/${username}";
+    sessionVariables = {
+      XDG_DATA_HOME = "${config.xdg.dataHome}";
+      XDG_CONFIG_HOME = "${config.xdg.configHome}";
+      XDG_STATE_HOME = "${config.xdg.stateHome}";
+      XDG_CACHE_HOME = "${config.xdg.cacheHome}";
+    };
   };
   programs.home-manager.enable = true;
   nixpkgs.config.allowUnfree = true;
