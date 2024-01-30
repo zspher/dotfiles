2aea80467f8e89d2de8dc7e4c767b2bf4ddc6cfe
zspher <66728045+zspher@users.noreply.github.com>
2024-01-30 15:14:59 +0000

    feat: use xdg.enable instead of sessionVars

--- configs/home-manager.nix
+++ configs/home-manager.nix
@@ -8,16 +8,11 @@ in {
   home = {
     inherit username;
     homeDirectory = "/home/${username}";
-    sessionVariables = {
-      XDG_DATA_HOME = "${config.xdg.dataHome}";
-      XDG_CONFIG_HOME = "${config.xdg.configHome}";
-      XDG_STATE_HOME = "${config.xdg.stateHome}";
-      XDG_CACHE_HOME = "${config.xdg.cacheHome}";
-    };
   };
   programs.home-manager.enable = true;
   nixpkgs.config.allowUnfree = true;
 
+  xdg.enable = true;
   xdg.userDirs.enable = true;
   xdg.userDirs.createDirectories = true;
 }
