0d31e519863fb359aa1d11ba5d0c1e9c1e11a823
zspher <66728045+zspher@users.noreply.github.com>
2024-01-31 17:00:36 +0000

    fix: obsidian electron version EOL

--- configs/home-manager.nix
+++ configs/home-manager.nix
@@ -1,6 +1,8 @@
 {
   config,
   data,
+  pkgs,
+  lib,
   ...
 }: let
   inherit (data) username;
@@ -11,6 +13,8 @@ in {
   };
   programs.home-manager.enable = true;
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.permittedInsecurePackages =
+    lib.optional (pkgs.obsidian.version == "1.5.3") "electron-25.9.0";
 
   xdg.enable = true;
   xdg.userDirs.enable = true;
