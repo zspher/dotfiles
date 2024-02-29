5b731d5d40fe20e6e9a43aa05c875d4d2503bf03
zspher <66728045+zspher@users.noreply.github.com>
2024-02-29 15:06:59 +0800

    feat(nixos): add pi machine deps

--- system/profiles/pi/default.nix
+++ system/profiles/pi/default.nix
@@ -1,6 +1,18 @@
 {...}: {
   imports = [
     ./hardware-config.nix
+
+    ../../nix
+
+    ../../fonts.nix
+    ../../security.nix
+    ../../user.nix
+
+    ../../programs
+
+    ../../networking
+    ../../networking/openssh.nix
+    ../../networking/syncting.nix
   ];
   networking.hostName = "ns-200";
 }
