2dd7b21a442448ed661171c797d3a643708a0c00
zspher <66728045+zspher@users.noreply.github.com>
2024-03-14 17:17:06 +0800

    feat: change syncthing user

--- system/networking/syncting.nix
+++ system/networking/syncting.nix
@@ -4,6 +4,9 @@ in {
   systemd.services.syncthing.serviceConfig.UMask = "0007";
   services.syncthing = {
     enable = true;
+    inherit user;
+    dataDir = "/home/${user}/Documents";
+    configDir = "/home/${user}/.config/syncthing";
     overrideDevices = false;
     overrideFolders = false;
     openDefaultPorts = true;
