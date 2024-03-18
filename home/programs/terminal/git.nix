17557d1cc7f89209dd37f8297c93c838811e9460
zspher <66728045+zspher@users.noreply.github.com>
2024-03-18 14:40:28 +0800

    feat(git): change signing key

--- home/programs/terminal/git.nix
+++ home/programs/terminal/git.nix
@@ -3,7 +3,7 @@
     enable = true;
     userName = "zspher";
     userEmail = "66728045+zspher@users.noreply.github.com";
-    signing.key = "D5FF4680";
+    signing.key = "63A15B00";
     signing.signByDefault = true;
     extraConfig = {
       core.editor = "nvim";
