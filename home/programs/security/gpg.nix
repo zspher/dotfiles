0cb3531664adc40ff57f372a348daec9e04789f2
zspher <66728045+zspher@users.noreply.github.com>
2024-03-23 22:19:34 +0800

    feat(hm): use services.gpg-agent.pinentryPackage

--- home/programs/security/gpg.nix
+++ home/programs/security/gpg.nix
@@ -1,10 +1,14 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   programs.gpg.enable = true;
   programs.gpg.homedir = "${config.xdg.dataHome}/gnupg";
 
   services.gnome-keyring.enable = true;
   services.gpg-agent = {
     enable = true;
-    pinentryFlavor = "qt";
+    pinentryPackage = pkgs.pinentry-qt;
   };
 }
