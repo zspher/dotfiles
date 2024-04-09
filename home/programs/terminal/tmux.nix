41ee9538c08c759756b64fc87c35b3a67f9b071d
zspher <66728045+zspher@users.noreply.github.com>
2024-04-09 20:22:18 +0800

    feat(tmux): sync clipboard & add yank keybinds

--- home/programs/terminal/tmux.nix
+++ home/programs/terminal/tmux.nix
@@ -5,7 +5,13 @@
     mouse = true;
     baseIndex = 1;
     keyMode = "vi";
+    extraConfig = ''
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+      bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+    '';
     plugins = with pkgs; [
+      tmuxPlugins.yank
     ];
   };
 }
