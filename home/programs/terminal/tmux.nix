b1d413d70661eabe3ed381cb36d181ccc589e35c
zspher <66728045+zspher@users.noreply.github.com>
2024-04-09 20:22:43 +0800

    feat(tmux): new pane in current path

--- home/programs/terminal/tmux.nix
+++ home/programs/terminal/tmux.nix
@@ -9,6 +9,9 @@
       bind-key -T copy-mode-vi v send-keys -X begin-selection
       bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
       bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+
+      bind '"' split-window -v -c "#{pane_current_path}"
+      bind % split-window -h -c "#{pane_current_path}"
     '';
     plugins = with pkgs; [
       tmuxPlugins.yank
