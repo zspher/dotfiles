052441785d9ae3408ceeddf59bfbb09f002a56fc
zspher <66728045+zspher@users.noreply.github.com>
2024-04-20 14:15:14 +0800

    feat(catppuccin/kde): use qt.kde.settings for setting theme

--- modules/catppuccin/kde.nix
+++ modules/catppuccin/kde.nix
@@ -9,6 +9,17 @@ with lib; let
   upperFirst = str: (lib.strings.toUpper (builtins.substring 0 1 str)) + (builtins.substring 1 veryBigNum str);
 
   cfg = config.theme.catppuccin;
+
+  fromINI = file: let
+    inherit (builtins) fromJSON readFile;
+
+    # convert to json
+    json = with pkgs;
+      runCommand "converted.json" {} ''
+        ${pkgs.jc}/bin/jc --ini < ${file} > $out
+      '';
+  in
+    fromJSON (readFile json);
 in {
   options.theme.catppuccin.kde = {
     enable = mkEnableOption "kde integration";
@@ -24,12 +35,16 @@ in {
 
     accent = upperFirst cfg.accent;
     variant = upperFirst cfg.variant;
+
+    themeInfo = lib.mkMerge [
+      (fromINI "${package}/share/color-schemes/Catppuccin${variant}${accent}.colors")
+    ];
   in
     mkIf cfg.kde.enable {
       home.packages = [
         package
       ];
 
-      xdg.configFile."kdeglobals".source = "${package}/share/color-schemes/Catppuccin${variant}${accent}.colors";
+      qt.kde.settings.kdeglobals = themeInfo;
     };
 }
