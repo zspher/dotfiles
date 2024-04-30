7c331d2c1b42e5be678dd05bb52c6fb0dd45d4d0
zspher <66728045+zspher@users.noreply.github.com>
2024-04-30 13:18:29 +0800

    fix(auto-cpufreq): `--force` not working
    - see https://github.com/AdnanHodzic/auto-cpufreq/issues/620

--- system/nix/nixpkgs.nix
+++ system/nix/nixpkgs.nix
@@ -1,10 +1,25 @@
-{
-  lib,
-  pkgs,
-  ...
-}: {
+{lib, ...}: {
   nixpkgs = {
-    overlays = [];
+    overlays = [
+      (final: prev: {
+        auto-cpufreq =
+          prev.auto-cpufreq.overrideAttrs
+          rec {
+            _version = "1.9.9";
+            version = lib.warnIf (prev.auto-cpufreq.version != _version) "Seems like auto-cpufreq has been updated!" _version;
+            postInstall = ''
+              # copy script manually
+              cp scripts/cpufreqctl.sh $out/bin/cpufreqctl.auto-cpufreq
+              # systemd service
+              mkdir -p $out/lib/systemd/system
+              cp scripts/auto-cpufreq.service $out/lib/systemd/system
+            '';
+            postPatch = ''
+              substituteInPlace auto_cpufreq/core.py --replace '/opt/auto-cpufreq/override.pickle' /var/run/override.pickle
+            '';
+          };
+      })
+    ];
     config = {
       allowUnfree = true;
       permittedInsecurePackages = [];
