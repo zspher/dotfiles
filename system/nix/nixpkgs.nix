213f0a909cb3b68dc85c372cf52b0d926ed51b0b
zspher <66728045+zspher@users.noreply.github.com>
2024-04-30 13:16:40 +0800

    feat: remove 0d31e519863f fix

--- system/nix/nixpkgs.nix
+++ system/nix/nixpkgs.nix
@@ -7,8 +7,7 @@
     overlays = [];
     config = {
       allowUnfree = true;
-      permittedInsecurePackages =
-        lib.optional (pkgs.obsidian.version == "1.5.3") "electron-25.9.0";
+      permittedInsecurePackages = [];
     };
   };
 }
