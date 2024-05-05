89d39fdb795db290f9e4434ebf31c3ac5a4aa519
zspher <66728045+zspher@users.noreply.github.com>
2024-05-05 23:44:52 +0800

    fix: catppuccin gtk theme not applying properly

--- flake.nix
+++ flake.nix
@@ -53,7 +53,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-parts.follows = "flake-parts";
     };
-    catppuccin.url = "github:catppuccin/nix";
+    # pinned until FIX: https://github.com/catppuccin/nix/pull/159
+    catppuccin.url = "github:catppuccin/nix/2788becbb58bd2a60666fbbf2d4f6ae1721112d5";
     spicetify-nix = {
       url = "github:the-argus/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
