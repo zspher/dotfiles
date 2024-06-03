9558ecafb1558500a1a7907ec7d89342e6809769
zspher <66728045+zspher@users.noreply.github.com>
2024-06-03 22:48:05 +0800

    feat(packages): use the same overlays

--- packages/default.nix
+++ packages/default.nix
@@ -1,9 +1,18 @@
 {
+  self,
+  lib,
+  ...
+}: {
   perSystem = {
     pkgs,
     inputs',
+    system,
     ...
   }: {
+    _module.args.pkgs = import self.inputs.nixpkgs {
+      inherit system;
+      inherit ((import ../system/nix/nixpkgs.nix {inherit self lib;}).nixpkgs) overlays config;
+    };
     packages = {
       catppuccin-obs = pkgs.callPackage ./catppuccin-obs {};
       posy-cursor = pkgs.callPackage ./posy-cursor {};
