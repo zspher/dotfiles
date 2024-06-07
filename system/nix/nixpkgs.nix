51db840eeecbb0d63ed53843763f19f017c5587c
zspher <66728045+zspher@users.noreply.github.com>
2024-06-07 11:16:29 +0800

    feat: remove auto-cpufreq fix; updated on nixpkgs

--- system/nix/nixpkgs.nix
+++ system/nix/nixpkgs.nix
@@ -9,24 +9,6 @@
   ];
   nixpkgs = {
     overlays = [
-      (final: prev: {
-        auto-cpufreq =
-          prev.auto-cpufreq.overrideAttrs
-          rec {
-            _version = "1.9.9";
-            version = lib.warnIf (prev.auto-cpufreq.version != _version) "Seems like auto-cpufreq has been updated!" _version;
-            postInstall = ''
-              # copy script manually
-              cp scripts/cpufreqctl.sh $out/bin/cpufreqctl.auto-cpufreq
-              # systemd service
-              mkdir -p $out/lib/systemd/system
-              cp scripts/auto-cpufreq.service $out/lib/systemd/system
-            '';
-            postPatch = ''
-              substituteInPlace auto_cpufreq/core.py --replace '/opt/auto-cpufreq/override.pickle' /var/run/override.pickle
-            '';
-          };
-      })
       inputs.waybar.overlays.default
     ];
     config = {
