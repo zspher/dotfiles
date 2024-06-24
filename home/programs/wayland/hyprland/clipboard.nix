4beab332d65d100c9154cba7b2f40c5d8435c0d9
zspher <66728045+zspher@users.noreply.github.com>
2024-06-24 14:14:06 +0800

    fix(hm.hyprland): clipboard wayland & xwayland not syncing

--- home/programs/wayland/hyprland/clipboard.nix
+++ home/programs/wayland/hyprland/clipboard.nix
@@ -10,9 +10,12 @@
 
   wayland.windowManager.hyprland.settings.exec-once = [
     (lib.concatStrings [
-      "wl-paste -t text -w "
-      "sh -c 'v=$(cat); cmp -s <(xclip -o)  <<< \"$v\" || "
-      "xclip <<< \"$v\"'"
+      # "wl-paste -t text -w "
+      # "sh -c 'v=$(cat); cmp -s <(xclip -o)  <<< \"$v\" || "
+      # "xclip <<< \"$v\"'"
+      "wl-paste -t text -w sh -c "
+      "'sleep 0.05 && [ \"$(xclip -selection clipboard -o)\" = \"$(wl-paste -n)\" ] "
+      "&& wl-copy \"$(xclip -o)\" || xclip -selection \"clipboard primary\"'"
     ])
   ];
 }
