82619d5e5d6983afe986839e244d7516dd7df15a
zspher <66728045+zspher@users.noreply.github.com>
2024-07-03 17:04:39 +0800

    feat(fzf): use catppuccin-nix to set colors

--- home/programs/terminal/fzf.nix
+++ home/programs/terminal/fzf.nix
@@ -4,19 +4,5 @@
     enableZshIntegration = false;
     changeDirWidgetCommand = "fd -L -td --hidden --exclude .git";
     fileWidgetCommand = "fd -L -td -tf --hidden --exclude .git";
-    colors = {
-      "bg+" = "#313244";
-      "bg" = "#1e1e2e";
-      "fg" = "#cdd6f4";
-      "fg+" = "#cdd6f4";
-      "hl" = "#f38ba8";
-      "hl+" = "#f38ba8";
-      "spinner" = "#f5e0dc";
-      "header" = "#f38ba8";
-      "info" = "#cba6f7";
-      "pointer" = "#f5e0dc";
-      "marker" = "#f5e0dc";
-      "prompt" = "#cba6f7";
-    };
   };
 }
