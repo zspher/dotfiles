234ad7a0da1805148413298fb595ca18cb170f12
zspher <66728045+zspher@users.noreply.github.com>
2024-07-06 17:11:03 +0800

    refactor(dolphin): move libsForQt5 to with

--- home/programs/utils/dolphin.nix
+++ home/programs/utils/dolphin.nix
@@ -1,11 +1,11 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [
-    libsForQt5.dolphin
-    libsForQt5.kio-extras # thumbnail service
+  home.packages = with pkgs.libsForQt5; [
+    dolphin
+    kio-extras # thumbnail service
     # https://invent.kde.org/frameworks/kimageformats
-    libsForQt5.kimageformats # avif, xcf, jxl
+    kimageformats # avif, xcf, jxl
     # https://apps.kde.org/kdegraphics_thumbnailers/
-    libsForQt5.kdegraphics-thumbnailers # PS, PDF, RAW, mobi, blender
-    libsForQt5.ffmpegthumbs # video thumbnails
+    kdegraphics-thumbnailers # PS, PDF, RAW, mobi, blender
+    ffmpegthumbs # video thumbnails
   ];
 }
