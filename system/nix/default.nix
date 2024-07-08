a62a468a3e2a53cf96438be4e4a21f5134d0c373
zspher <66728045+zspher@users.noreply.github.com>
2024-07-08 17:54:10 +0800

    feat(nixos): switch nix.gc.automatic to nh.clean

--- system/nix/default.nix
+++ system/nix/default.nix
@@ -10,16 +10,16 @@
   nix = {
     enable = true;
     package = pkgs.nix;
-    gc =
-      {
-        automatic = true;
-        options = "--delete-older-than 1w";
-      }
-      // (
-        if builtins.hasAttr "home" config
-        then {frequency = "weekly";}
-        else {dates = "weekly";}
-      );
+    # gc =
+    #   {
+    #     automatic = true;
+    #     options = "--delete-older-than 1w";
+    #   }
+    #   // (
+    #     if builtins.hasAttr "home" config
+    #     then {frequency = "weekly";}
+    #     else {dates = "weekly";}
+    #   );
     settings = {
       experimental-features = ["nix-command" "flakes"];
       auto-optimise-store = true;
--- system/programs/nh.nix
+++ system/programs/nh.nix
@@ -3,6 +3,8 @@
 in {
   programs.nh = {
     enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep 2 --keep-since 3d --nogcroots";
     flake = "/home/${username}/dotfiles";
   };
 }
