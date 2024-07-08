c0c5f6b669eb614e93e976ae6d40c54c08497b0f
zspher <66728045+zspher@users.noreply.github.com>
2024-07-08 17:37:24 +0800

    feat(sddm): use qt6

--- system/services/sddm.nix
+++ system/services/sddm.nix
@@ -7,9 +7,10 @@
     enable = true;
     theme = "sddm-corners";
     wayland.enable = true;
+    package = pkgs.kdePackages.sddm;
   };
 
-  environment.systemPackages = with pkgs; [
-    self.packages.${pkgs.system}.sddm-corners-theme
+  environment.systemPackages = [
+    (self.packages.${pkgs.system}.sddm-corners-theme.override {font = "CaskaydiaMono Nerd Font";})
   ];
 }
