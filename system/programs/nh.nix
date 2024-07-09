5e05eb88a2b7715b20dd6ca925e4d945fdf3fb78
zspher <66728045+zspher@users.noreply.github.com>
2024-07-10 02:24:03 +0800

    feat(nh): clean remove since

--- system/programs/nh.nix
+++ system/programs/nh.nix
@@ -4,7 +4,7 @@ in {
   programs.nh = {
     enable = true;
     clean.enable = true;
-    clean.extraArgs = "--keep 2 --keep-since 3d --nogcroots";
+    clean.extraArgs = "--keep 2 --nogcroots";
     flake = "/home/${username}/dotfiles";
   };
 }
