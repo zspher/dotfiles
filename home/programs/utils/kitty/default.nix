71d8f6fcb9c76d1747905160729347acc2e59f7b
zspher <66728045+zspher@users.noreply.github.com>
2024-07-20 01:29:15 +0800

    refactor(kitty): remove already default options

--- home/programs/utils/kitty/default.nix
+++ home/programs/utils/kitty/default.nix
@@ -2,7 +2,7 @@
   programs.kitty = {
     enable = true;
     font.name = "CaskaydiaMono Nerd Font";
-    shellIntegration.mode = "no-rc no-title";
+    shellIntegration.mode = "no-title";
     settings = {
       disable_ligatures = "always";
 
