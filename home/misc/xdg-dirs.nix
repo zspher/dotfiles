f94911673c6657be3924cb161a70acc30e08314d
zspher <66728045+zspher@users.noreply.github.com>
2024-07-30 20:23:24 +0800

    feat(hm): ensure xdg dir compliance

--- home/misc/xdg-dirs.nix
+++ home/misc/xdg-dirs.nix
@@ -3,6 +3,7 @@
     CARGO_HOME = "${config.xdg.dataHome}/cargo";
     CUDA_CACHE_PATH = "${config.xdg.cacheHome}/nv";
     DOTNET_CLI_HOME = "${config.xdg.dataHome}/dotnet";
+    GOPATH = "${config.xdg.dataHome}/go";
     OMNISHARPHOME = "${config.xdg.configHome}/omnisharp";
     TERMINFO = "${config.xdg.dataHome}/terminfo";
     TERMINFO_DIRS = "${config.xdg.dataHome}/terminfo\${TERMINFO_DIRS:+:$TERMINFO_DIRS}";
