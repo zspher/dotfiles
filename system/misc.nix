e98f2650a8f478c7da90108c707ef2e20ec74f48
zspher <66728045+zspher@users.noreply.github.com>
2024-08-05 21:27:56 +0800

    feat(nixos): add locale & catppuccin theme console

--- system/misc.nix
+++ system/misc.nix
@@ -1,6 +1,25 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  imports = [
+    inputs.catppuccin.nixosModules.catppuccin
+  ];
+
   # fonts
   fonts.packages = with pkgs; [
     (nerdfonts.override {fonts = ["CascadiaMono" "Noto"];})
   ];
+
+  # internationalization
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_TIME = "C.UTF-8";
+  };
+
+  # theme
+  catppuccin.flavor = "mocha";
+  catppuccin.accent = "mauve";
+  console.catppuccin.enable = true;
 }
