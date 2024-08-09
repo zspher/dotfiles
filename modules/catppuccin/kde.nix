c64b8e53f8c21f652e95773f7a6665f8316aae0d
zspher <66728045+zspher@users.noreply.github.com>
2024-08-09 21:17:30 +0800

    refactor(catppuccin.kde): make some things clearer
    - remove with lib;
    - use lib.importJSON
    - simplify code

--- modules/catppuccin/kde.nix
+++ modules/catppuccin/kde.nix
@@ -3,8 +3,7 @@
   lib,
   pkgs,
   ...
-}:
-with lib; let
+}: let
   upperFirst = str:
     (lib.toUpper (builtins.substring 0 1 str))
     + (builtins.substring 1 (builtins.stringLength str) str);
@@ -12,19 +11,15 @@ with lib; let
   cfg = config.theme.catppuccin;
   ctp = {inherit (config.catppuccin) sources flavor accent;};
 
-  fromINI = file: let
-    inherit (builtins) fromJSON readFile;
-
-    # convert to json
-    json = with pkgs;
-      runCommand "converted.json" {} ''
-        ${pkgs.jc}/bin/jc --ini < ${file} > $out
-      '';
+  importINI = file: let
+    json = pkgs.runCommand "INItoJSON" {} ''
+      ${pkgs.jc}/bin/jc --ini < ${file} > $out
+    '';
   in
-    fromJSON (readFile json);
+    lib.importJSON json;
 in {
   options.theme.catppuccin.kde = {
-    enable = mkEnableOption "kde integration";
+    enable = lib.mkEnableOption "kde integration";
   };
 
   config = let
@@ -37,11 +32,11 @@ in {
     flavor = upperFirst ctp.flavor;
 
     themeInfo = lib.mkMerge [
-      (fromINI "${package}/share/color-schemes/Catppuccin${flavor}${accent}.colors")
+      (importINI "${package}/share/color-schemes/Catppuccin${flavor}${accent}.colors")
       {"ColorEffects:Disabled".IntensityAmount = lib.mkForce 1;}
     ];
   in
-    mkIf cfg.kde.enable {
+    lib.mkIf cfg.kde.enable {
       home.packages = [
         package # for krita, okular
       ];
