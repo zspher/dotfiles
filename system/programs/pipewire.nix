84d1bc513e9da0ba8da50be1491f84abf2aa8cfe
zspher <66728045+zspher@users.noreply.github.com>
2024-08-09 14:00:31 +0800

    fix(nixos): pipewire draining power

--- system/programs/pipewire.nix
+++ system/programs/pipewire.nix
@@ -5,5 +5,15 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+
+    # BUG: https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/2669
+    # https://www.reddit.com/r/linux/comments/1em8biv/psa_pipewire_has_been_halving_your_battery_life/
+    wireplumber.extraConfig = {
+      "10-disable-camera" = {
+        "wireplumber.profiles" = {
+          main."monitor.libcamera" = "disabled";
+        };
+      };
+    };
   };
 }
