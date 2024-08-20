1cb3dda9b93134f28e004f95834ed027e038dafc
zspher <66728045+zspher@users.noreply.github.com>
2024-08-20 17:10:09 +0800

    feat(hyprland/clipboard): disable wl-paste xwayland sync
    - does not work properly now

--- home/programs/wayland/hyprland/clipboard.nix
+++ home/programs/wayland/hyprland/clipboard.nix
@@ -10,13 +10,15 @@
     zbar
   ];
 
-  wayland.windowManager.hyprland.settings.exec-once = [
-    (lib.concatStrings [
-      # wayland <-> xwayand primary is enabled by default in hyprland
-      # wayland <-> xwayand clipboard
-      "wl-paste -t text -w sh -c "
-      "'sleep 0.05 && [ \"$(xclip -selection clipboard -o)\" = \"$(wl-paste -n)\" ] "
-      "|| xclip -selection clipboard'"
-    ])
-  ];
+  wayland.windowManager.hyprland.settings.exec-once = ["${pkgs.copyq}/bin/copyq --start-server"];
+
+  # wayland.windowManager.hyprland.settings.exec-once = [
+  #   (lib.concatStrings [
+  #     # wayland <-> xwayand primary is enabled by default in hyprland
+  #     # wayland <-> xwayand clipboard
+  #     "wl-paste -t text -w sh -c "
+  #     "'sleep 0.05 && [ \"$(xclip -selection clipboard -o)\" = \"$(wl-paste -n)\" ] "
+  #     "|| xclip -selection clipboard'"
+  #   ])
+  # ];
 }
