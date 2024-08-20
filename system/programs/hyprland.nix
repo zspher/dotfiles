54db0a1741ae5fc81b8b011d0a1205e09f04cc88
zspher <66728045+zspher@users.noreply.github.com>
2024-08-20 23:48:33 +0800

    feat(nixos): add qt6 qtwayland when hyprland

--- system/programs/hyprland.nix
+++ system/programs/hyprland.nix
@@ -1,7 +1,14 @@
-{inputs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   imports = [
     # inputs.hyprland.nixosModules.default
   ];
   programs.hyprland.enable = true;
   environment.sessionVariables.NIXOS_OZONE_WL = 1;
+  environment.systemPackages = with pkgs; [
+    kdePackages.qtwayland
+  ];
 }
