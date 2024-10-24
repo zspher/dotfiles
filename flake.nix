596098a623dad7e0e9f5bc6ebabeeb60aac03704
zspher <66728045+zspher@users.noreply.github.com>
2024-10-24 23:04:17 +0800

    chore: update flake.lock

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
     };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=3f16bb4a40a55753cb84be9a54cd42c16222de6e";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # hyprland = {
     #   url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     #   inputs.nixpkgs.follows = "nixpkgs";
