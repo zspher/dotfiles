5aa2ea996efda2f2ca0bbb99ee4138946541c20c
zspher <66728045+zspher@users.noreply.github.com>
2024-11-03 14:24:40 -0700

    fix: gtk4 applications not running when vulkan not found

--- system/programs/hyprland.nix
+++ system/programs/hyprland.nix
@@ -9,6 +9,7 @@
   ];
   programs.hyprland.enable = true;
   environment.sessionVariables.NIXOS_OZONE_WL = 1;
+  environment.sessionVariables.GSK_RENDERER = "ngl";
   environment.systemPackages = with pkgs; [
     kdePackages.qtwayland
   ];
