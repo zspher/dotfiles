3aa388f52f50b4b8bc28063e5f973ef8999df28b
zspher <66728045+zspher@users.noreply.github.com>
2024-11-04 14:51:24 -0700

    feat: remove uneeded fixes

--- system/programs/pipewire.nix
+++ system/programs/pipewire.nix
@@ -6,16 +6,5 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
-
-    # # BUG: https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/2669
-    # #      https://www.reddit.com/r/linux/comments/1em8biv/psa_pipewire_has_been_halving_your_battery_life/
-    # #       waiting for staging-next
-    # wireplumber.extraConfig = {
-    #   "10-disable-camera" = {
-    #     "wireplumber.profiles" = {
-    #       main."monitor.libcamera" = "disabled";
-    #     };
-    #   };
-    # };
   };
 }
