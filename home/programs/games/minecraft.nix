2238811670b0ce925c45ad1427513aa9c7c1147f
zspher <66728045+zspher@users.noreply.github.com>
2024-11-16 17:32:53 -0700

    feat(minecraft): add `jdk21`

--- home/programs/games/minecraft.nix
+++ home/programs/games/minecraft.nix
@@ -5,6 +5,12 @@
 }:
 {
   home.packages = [
-    inputs.pollymc.packages.${pkgs.system}.pollymc
+    (inputs.pollymc.packages.${pkgs.system}.pollymc.override {
+      jdks = with pkgs; [
+        jdk8
+        jdk17
+        jdk21
+      ];
+    })
   ];
 }
