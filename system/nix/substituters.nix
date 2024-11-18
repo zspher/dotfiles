b9acc9440f72df0c388574765c11d33192a5ad52
zspher <66728045+zspher@users.noreply.github.com>
2024-11-18 14:24:49 -0700

    feat: update substituters

--- system/nix/substituters.nix
+++ system/nix/substituters.nix
@@ -5,13 +5,13 @@
       "https://cache.nixos.org?priority=10"
       "https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
-      "https://walker.cachix.org"
+      "https://walker-git.cachix.org"
     ];
     trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
+      "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
     ];
   };
 }
