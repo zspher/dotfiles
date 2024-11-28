98506a1778224b8616d39553074c9a6201d6871d
zspher <66728045+zspher@users.noreply.github.com>
2024-11-27 23:54:39 -0700

    Revert "fix: libreoffice build error"
    This reverts commit 1d5d42dbf13da055c8000f628dceb2cfed845ba6.

--- home/programs/documents/default.nix
+++ home/programs/documents/default.nix
@@ -2,7 +2,7 @@
 {
   home.packages = with pkgs; [
     calibre
-    # libreoffice-fresh # TODO: see https://github.com/NixOS/nixpkgs/issues/357387
+    libreoffice-fresh
     masterpdfeditor4
     obsidian
     kdePackages.okular
