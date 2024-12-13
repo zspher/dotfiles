cc2f37ce15e62169801d28170e48adc531c7096f
zspher <66728045+zspher@users.noreply.github.com>
2024-12-12 22:44:58 -0700

    feat: disable kdeconnect overlay

--- system/nix/nixpkgs.nix
+++ system/nix/nixpkgs.nix
@@ -7,17 +7,17 @@
 {
   nixpkgs = {
     overlays = [
-      (final: prev: {
-        kdePackages = prev.kdePackages.overrideScope (
-          selfx: prevx: {
-            kdeconnect-kde = prevx.kdeconnect-kde.overrideAttrs (oldAttrs: {
-              cmakeFlags = oldAttrs.cmakeFlags ++ [
-                "-DMDNS_ENABLED=OFF"
-              ];
-            });
-          }
-        );
-      })
+      # (final: prev: {
+      #   kdePackages = prev.kdePackages.overrideScope (
+      #     selfx: prevx: {
+      #       kdeconnect-kde = prevx.kdeconnect-kde.overrideAttrs (oldAttrs: {
+      #         cmakeFlags = oldAttrs.cmakeFlags ++ [
+      #           "-DMDNS_ENABLED=OFF"
+      #         ];
+      #       });
+      #     }
+      #   );
+      # })
     ];
     config = {
       allowUnfree = true;
