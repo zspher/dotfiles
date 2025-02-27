f3988361d62badf0839b936028cef9a63e98ccee
zspher <66728045+zspher@users.noreply.github.com>
2025-02-27 00:09:33 -0700

    fix: biome build failure

--- system/nix/nixpkgs.nix
+++ system/nix/nixpkgs.nix
@@ -19,6 +19,20 @@
       #     }
       #   );
       # })
+      (final: prev: {
+        biome = prev.biome.overrideAttrs (oldAttrs: {
+          # FIX: https://github.com/NixOS/nixpkgs/issues/384795
+          # from https://github.com/NixOS/nixpkgs/pull/384904
+          cargoTestFlags = oldAttrs.cargoBuildFlags ++ [
+            "-- --skip=commands::check::print_json"
+            "--skip=commands::check::print_json_pretty"
+            "--skip=commands::explain::explain_logs"
+            "--skip=commands::format::print_json"
+            "--skip=commands::format::print_json_pretty"
+            "--skip=commands::format::should_format_files_in_folders_ignored_by_linter"
+          ];
+        });
+      })
     ];
     config = {
       allowUnfree = true;
