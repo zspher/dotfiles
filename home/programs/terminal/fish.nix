14c0e6d7cde8443b771debc4905e154269a3493c
zspher <66728045+zspher@users.noreply.github.com>
2025-03-06 20:06:03 -0700

    feat(fish): update keybinds now that 4.0 has dropped

--- home/programs/terminal/fish.nix
+++ home/programs/terminal/fish.nix
@@ -25,27 +25,17 @@
     shellInit = '''';
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
-
-      # cursor
-      set fish_cursor_default block
-      set fish_cursor_insert line
-      set fish_cursor_replace_one underscore
-      set fish_cursor_replace underscore
-      set fish_cursor_external line
-      set fish_cursor_visual block
     '';
     functions = {
       fish_user_key_bindings = {
         body = ''
           fish_vi_key_bindings
 
-          bind -e --key \cy
+          bind -M insert ctrl-y accept-autosuggestion
+          bind -M insert alt-n forward-word
 
-          bind -M insert \cy accept-autosuggestion
-          bind -M insert \en forward-word
-
-          bind -M insert \cn down-or-search
-          bind -M insert \cp up-or-search
+          bind -M insert ctrl-n down-or-search
+          bind -M insert ctrl-p up-or-search
         '';
       };
     };
