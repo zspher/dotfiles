e2e0c1a7154e4e6e239ad5cc205a15a85e8f096b
zspher <66728045+zspher@users.noreply.github.com>
2025-04-02 10:57:10 -0600

    feat: change locale
    - YY-MM-DD 24hr
    - canadian locale

--- system/misc.nix
+++ system/misc.nix
@@ -15,9 +15,18 @@
   ];
 
   # internationalization
-  i18n.defaultLocale = "en_GB.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_TIME = "C.UTF-8";
+  i18n = {
+    defaultLocale = "en_CA.UTF-8";
+    extraLocaleSettings = {
+      LC_TIME = "en_DK.UTF-8";
+    };
+    supportedLocales = [
+      "C.UTF-8/UTF-8"
+      "en_US.UTF-8/UTF-8"
+      "en_GB.UTF-8/UTF-8"
+      "en_CA.UTF-8/UTF-8"
+      "en_DK.UTF-8/UTF-8"
+    ];
   };
 
   boot.supportedFilesystems = {
