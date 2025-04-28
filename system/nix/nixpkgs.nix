b6afefaa74461cebfd4b5d157f87a70fec2d53a2
zspher <66728045+zspher@users.noreply.github.com>
2025-04-27 18:38:10 -0600

    Revert "fix: copyq build failure"
    This reverts commit 5466e2be30aec5ef1d0bbd278bb87c46b5650c48.

--- system/nix/nixpkgs.nix
+++ system/nix/nixpkgs.nix
@@ -20,25 +20,6 @@
       #   );
       # })
       (final: prev: {
-        copyq = prev.copyq.overrideAttrs (oldAttrs: rec {
-          version = "10.0.0";
-          src = prev.fetchFromGitHub {
-            owner = "hluk";
-            repo = "CopyQ";
-            rev = "v${version}";
-            hash = "sha256-lH3WJ6cK2eCnmcLVLnYUypABj73UZjGqqDPp92QE+V4=";
-          };
-          nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ prev.pkg-config ];
-          patches = [
-            (prev.fetchpatch {
-              # Can be removed after next release
-              name = "fix-qchar-construction-for-qt-6.9.patch";
-              url = "https://github.com/hluk/CopyQ/commit/f08c0d46a239362c5d3525ef9c3ba943bb00f734.patch";
-              hash = "sha256-dsDIUVJHFFqzZ3tFOcYdwol/tm4viHM0CRs6wYfVKbQ=";
-            })
-          ];
-
-        });
       })
     ];
     config = {
