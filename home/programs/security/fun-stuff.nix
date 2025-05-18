7eff0884b7d82252b690700b32d231f9649cbe90
zspher <66728045+zspher@users.noreply.github.com>
2025-05-18 11:15:49 -0600

    feat: add reverse engineering utils

--- home/programs/security/fun-stuff.nix
+++ home/programs/security/fun-stuff.nix
@@ -1,9 +1,20 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+    # passwords
     (hashcat.override { cudaSupport = true; })
 
+    # network
     nmap
     wireshark
+
+    # reverse engineering
+    (cutter.withPlugins (
+      ps: with ps; [
+        jsdec
+        rz-ghidra
+        sigdb
+      ]
+    ))
   ];
 }
