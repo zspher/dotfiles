0a025035bd80d026ee29d0949599e3e4d4c45fc1
zspher <66728045+zspher@users.noreply.github.com>
2025-05-25 20:47:28 -0600

    feat: copy vesktop config to nix

--- home/programs/web/vesktop.nix
+++ home/programs/web/vesktop.nix
@@ -5,15 +5,36 @@
     vencord.settings = {
       autoUpdate = false;
       autoUpdateNotification = false;
-      notifyAboutUpdates = false;
+      notifyAboutUpdates = true;
       useQuickCss = true;
       disableMinSize = true;
+
+      eagerPatches = false;
+      enableReactDevtools = false;
+      frameless = false;
+      transparent = false;
+      winCtrlQ = false;
+      winNativeTitleBar = false;
+
       plugins = {
         MessageLogger = {
           enabled = true;
           ignoreSelf = true;
         };
         FakeNitro.enabled = true;
+        CrashHandler.enable = true;
+        WebKeybinds.enable = true;
+        WebScreenShareFixes.enable = true;
+        BiggerStreamPreview.enable = true;
+        ReverseImageSearch.enable = true;
+        Experiments.enable = true;
+      };
+
+      notifications = {
+        timeout = 5000;
+        position = "bottom-right";
+        useNative = "not-focused";
+        logLimit = 50;
       };
     };
   };
