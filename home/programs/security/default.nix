4d44137d921a8d84295796f8b17b2fe736545f03
zspher <66728045+zspher@users.noreply.github.com>
2025-05-26 20:35:37 -0600

    feat: switch kde-polkit-agent to polkit-gnome

--- home/programs/security/default.nix
+++ home/programs/security/default.nix
@@ -1,10 +1,11 @@
 { pkgs, ... }:
 {
   imports = [
-    ./kde-polkit-agent.nix
+    # ./kde-polkit-agent.nix
     ./gpg.nix
   ];
   home.packages = with pkgs; [
     keepassxc
   ];
+  services.polkit-gnome.enable = true;
 }
