6093a6b456f62a8e726255b4f1d9244ac1da6f87
zspher <66728045+zspher@users.noreply.github.com>
2025-06-03 20:49:22 -0600

    feat(vm): properly enable ovmf in qemu

--- system/programs/vm.nix
+++ system/programs/vm.nix
@@ -1,14 +1,14 @@
 { pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    OVMF
-  ];
   # virtualisation.virtualbox.guest.enable = true;
   # virtualisation.virtualbox.host.enable = true;
   virtualisation.spiceUSBRedirection.enable = true;
-  virtualisation.libvirtd.enable = true;
-  virtualisation.libvirtd.qemu.vhostUserPackages = with pkgs; [
-    virtiofsd
-  ];
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu.ovmf.enable = true;
+    qemu.vhostUserPackages = with pkgs; [
+      virtiofsd
+    ];
+  };
   programs.virt-manager.enable = true;
 }
