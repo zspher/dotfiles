25a7b5e483de8af1e5c205add3685ac1650376c5
zspher <66728045+zspher@users.noreply.github.com>
2025-06-13 23:28:45 -0600

    feat(calibre): add native dialogs

--- system/nix/nixpkgs.nix
+++ system/nix/nixpkgs.nix
@@ -24,6 +24,32 @@
         rizinPlugins = lib.recurseIntoAttrs rizin.plugins;
         cutter = pkgs.qt6.callPackage ../../packages/rizin/cutter.nix { inherit rizin; };
         cutterPlugins = lib.recurseIntoAttrs cutter.plugins;
+
+        calibre = prev.calibre.overrideAttrs (oldAttrs: {
+          patches = oldAttrs.patches ++ [
+            ./calibre-native-dialogs.patch
+          ];
+          preFixup =
+            let
+              popplerArgs = "--prefix PATH : ${prev.poppler-utils.out}/bin";
+            in
+            ''
+              for program in $out/bin/*; do
+                wrapProgram $program \
+                  ''${qtWrapperArgs[@]} \
+                  ''${gappsWrapperArgs[@]} \
+                  --prefix PATH : ${
+                    lib.makeBinPath [
+                      prev.libjpeg
+                      prev.libwebp
+                      prev.optipng
+                      prev.zenity
+                    ]
+                  } \
+                  ${if true then popplerArgs else ""}
+              done
+            '';
+        });
       })
     ];
     config = {
