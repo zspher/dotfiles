589b024f7ce22c63d76ad18e1446d4cca4379fe4
zspher <66728045+zspher@users.noreply.github.com>
2025-06-26 13:12:06 -0600

    feat(nixos): add flatpak

--- system/profiles/laptop/default.nix
+++ system/profiles/laptop/default.nix
@@ -13,6 +13,7 @@
     ../../programs
     ../../programs/blueman.nix
     ../../programs/container.nix
+    ../../programs/flatpak.nix
     ../../programs/hyprland.nix
     ../../programs/kanata.nix
     ../../programs/pipewire.nix
