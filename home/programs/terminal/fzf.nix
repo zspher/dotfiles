c253d5576c8c23faa9d196b5e5948905637a4036
zspher <66728045+zspher@users.noreply.github.com>
2025-07-09 23:27:12 -0600

    feat: fzf custom command no longer needed
    - hidden files & excluded .git are default in fzf with fzf-walker

--- home/programs/terminal/fzf.nix
+++ home/programs/terminal/fzf.nix
@@ -2,7 +2,5 @@
 {
   programs.fzf = {
     enable = true;
-    changeDirWidgetCommand = "fd -L -td --hidden --exclude .git";
-    fileWidgetCommand = "fd -L -td -tf --hidden --exclude .git";
   };
 }
