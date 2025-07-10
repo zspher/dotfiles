fb3dbea4a880841d7f860831b1f70d2d6ad44871
zspher <66728045+zspher@users.noreply.github.com>
2025-07-09 23:28:19 -0600

    feat(fish): add cd to projects
    - projects a more focused set of dirs

--- home/programs/terminal/fish.nix
+++ home/programs/terminal/fish.nix
@@ -27,6 +27,17 @@
       set fish_greeting # Disable greeting
     '';
     functions = {
+      fzf-project-search = {
+        body = ''
+          set -l query (commandline -b)
+          if set -l result (fd -t d --exact-depth 1 . ~/personal/ ~/Documents/ ~/personal/test ~/.config | fzf --query=$query)
+              cd -- $result
+              commandline -r -- ""
+          end
+
+          commandline -f repaint
+        '';
+      };
       fish_user_key_bindings = {
         body = ''
           fish_vi_key_bindings
@@ -36,6 +47,9 @@
 
           bind -M insert ctrl-n down-or-search
           bind -M insert ctrl-p up-or-search
+
+          bind -M insert ctrl-f fzf-project-search
+          bind ctrl-f fzf-project-search
         '';
       };
     };
