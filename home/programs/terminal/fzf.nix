2369905834e28034e41e83fc4fae3b301ed15c63
zspher <66728045+zspher@users.noreply.github.com>
2025-08-26 20:41:28 -0600

    feat(fzf): skip some dirs

--- home/programs/terminal/fzf.nix
+++ home/programs/terminal/fzf.nix
@@ -1,6 +1,12 @@
 { ... }:
 {
-  programs.fzf = {
-    enable = true;
-  };
+  programs.fzf =
+    let
+      skipDirs = "--walker-skip .git,node_modules,.cache,.direnv,.steam,.local";
+    in
+    {
+      enable = true;
+      changeDirWidgetOptions = [ skipDirs ];
+      fileWidgetOptions = [ skipDirs ];
+    };
 }
