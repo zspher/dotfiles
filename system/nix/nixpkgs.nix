dc5606b356365883ca7bcb7b04237754c9c2290f
zspher <66728045+zspher@users.noreply.github.com>
2025-09-18 22:40:53 -0600

    feat: use latest version on grimblast & copyq

--- system/nix/nixpkgs.nix
+++ system/nix/nixpkgs.nix
@@ -25,6 +25,27 @@
         });
         # .net 10 broke things
         roslyn-ls = inputs.nixpkgs-old-roslyn-ls.legacyPackages.${prev.system}.roslyn-ls;
+
+        # updates because I can't wait
+        grimblast = prev.grimblast.overrideAttrs (oldAttrs: {
+          version = "0.1-unstable-2025-09-16";
+          src = prev.fetchFromGitHub {
+            owner = "hyprwm";
+            repo = "contrib";
+            rev = "9e73a0e753251fbc6e987bd13324886dc6ca8f9a";
+            hash = "sha256-0TCggLT5bUMpJYoA8+EXs8Qu+D9sTVe6eOmsNetH8OI=";
+          };
+        });
+        copyq = prev.copyq.overrideAttrs (oldAttrs: rec {
+          version = "11.0.0";
+          src = prev.fetchFromGitHub {
+            owner = "hluk";
+            repo = "CopyQ";
+            rev = "v${version}";
+            hash = "sha256-/t+8YsqeX0tlxwQDDNTalttCDIgGhpLbzYe3UqY04xM=";
+          };
+          patches = [ ];
+        });
       })
     ];
     config = {
