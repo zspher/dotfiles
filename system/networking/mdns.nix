a445be6ce480d6df43a06b43a70e0a4ad66fe1c7
zspher <66728045+zspher@users.noreply.github.com>
2025-10-18 22:08:36 -0600

    feat: switch to avahi
    - can't detect network printers & scanners on systemd-resolved

--- system/networking/mdns.nix
+++ system/networking/mdns.nix
@@ -1,6 +1,7 @@
 { ... }:
 {
-  services.resolved.enable = true;
+  services.avahi.enable = true;
+  services.avahi.nssmdns4 = true;
   networking.networkmanager.connectionConfig."connection.mdns" = 2;
   networking.firewall.allowedUDPPorts = [
     5353
