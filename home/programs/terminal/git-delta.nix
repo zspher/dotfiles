9691fa2744c85dca2f41dffa32008fea6da0f2a4
zspher <66728045+zspher@users.noreply.github.com>
2025-10-26 17:48:32 -0600

    feat(git): use new module options

--- home/programs/terminal/git-delta.nix
+++ home/programs/terminal/git-delta.nix
@@ -1,9 +1,10 @@
 { pkgs, ... }:
 {
-  programs.git.delta = {
+  programs.delta = {
     enable = true;
     options.navigate = true;
     options.hunk-header-style = "omit";
+    enableGitIntegration = true;
   };
   programs.lazygit.settings = {
     git.paging.pager = "delta --dark --paging=never";
--- home/programs/terminal/git.nix
+++ home/programs/terminal/git.nix
@@ -2,16 +2,16 @@
 {
   programs.git = {
     enable = true;
-    userName = "zspher";
-    userEmail = "66728045+zspher@users.noreply.github.com";
-    signing.key = "63A15B00";
-    signing.signByDefault = true;
-    extraConfig = {
+    settings = {
+      user.name = "zspher";
+      user.email = "66728045+zspher@users.noreply.github.com";
       core.editor = "nvim";
       init.defaultBranch = "main";
       status.short = true;
       diff.colorMoved = true;
     };
+    signing.key = "63A15B00";
+    signing.signByDefault = true;
     ignores = [
       ".env"
       ".direnv"
