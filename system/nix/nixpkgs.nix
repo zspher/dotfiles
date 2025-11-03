41db66ae28c174a6f498d2dde8a816b3e83732fc
zspher <66728045+zspher@users.noreply.github.com>
2025-11-03 16:05:22 -0700

    feat(system): remove uneeded patches

--- system/nix/nixpkgs.nix
+++ system/nix/nixpkgs.nix
@@ -24,8 +24,6 @@
         bottles-unwrapped = prev.bottles-unwrapped.overrideAttrs (oldAttrs: {
           propagatedBuildInputs = lib.lists.remove prev.gamescope oldAttrs.propagatedBuildInputs;
         });
-        # .net 10 broke things
-        roslyn-ls = inputs.nixpkgs-old-roslyn-ls.legacyPackages.${prev.system}.roslyn-ls;
 
         wl-clipboard = prev.wl-clipboard.overrideAttrs (oldAttrs: {
           src = prev.fetchFromGitHub {
@@ -35,19 +33,6 @@
             hash = "sha256-V8JAai4gZ1nzia4kmQVeBwidQ+Sx5A5on3SJGSevrUU=";
           };
         });
-
-        # updates because I can't wait
-        zathuraPkgs = prev.zathuraPkgs.overrideScope (
-          selfx: prevx: {
-            zathura_core = prevx.zathura_core.overrideAttrs (oldAttrs: rec {
-              version = "0.5.13";
-              src = prev.fetchurl {
-                url = "https://pwmt.org/projects/zathura/download/zathura-${version}.tar.xz";
-                hash = "sha256-YwIXO81G+JflIJyIOltRrR2rSUbC84YcujdKO4DY88E=";
-              };
-            });
-          }
-        );
 
       })
     ];
