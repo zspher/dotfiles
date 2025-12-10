05e1eefb541443abb3816771a01e1c750403d7c4
zspher <66728045+zspher@users.noreply.github.com>
2025-12-10 16:04:21 -0700

    feat: allow warframe udp ports

--- system/networking/network-manager.nix
+++ system/networking/network-manager.nix
@@ -2,4 +2,9 @@
 {
   networking.networkmanager.enable = true;
   networking.networkmanager.wifi.powersave = true;
+  networking.firewall.allowedUDPPorts = [
+    #warframe
+    4950
+    4955
+  ];
 }
