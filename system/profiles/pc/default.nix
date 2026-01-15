8644ea2a9d2fb630e07591159bda1763ef59c29d
zspher <66728045+zspher@users.noreply.github.com>
2026-01-14 22:35:08 -0700

    feat(system): update c-100 config for new pc

--- system/profiles/pc/default.nix
+++ system/profiles/pc/default.nix
@@ -11,12 +11,15 @@
 
     ../../programs
     ../../programs/blueman.nix
+    ../../programs/container.nix
+    ../../programs/flatpak.nix
     ../../programs/hyprland.nix
     ../../programs/kanata.nix
     ../../programs/pipewire.nix
     ../../programs/sddm.nix
     ../../programs/steam.nix
     ../../programs/storage-dev.nix
+    ../../programs/vm.nix
 
     ../../networking
     ../../networking/openssh.nix
--- system/profiles/pc/hardware-config.nix
+++ system/profiles/pc/hardware-config.nix
@@ -1,73 +1,114 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 {
   imports = [
     ../../hardware
     ../../hardware/bluetooth.nix
+    ../../hardware/printer-scanner.nix
   ];
   boot = {
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
 
     initrd.availableKernelModules = [
-      "xhci_pci"
-      "ehci_pci"
       "ahci"
+      "nvme"
+      "sd_mod"
       "usb_storage"
       "usbhid"
-      "sd_mod"
+      "xhci_pci"
     ];
     kernelModules = [ "kvm-intel" ];
+    blacklistedKernelModules = [
+      "iTCO_wdt"
+    ];
+
+    kernelParams = [
+      "intel_iommu=on"
+      "nmi_watchdog=0"
+    ];
   };
 
   # hardware
-  fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-label/nixos";
-      fsType = "btrfs";
-      options = [
-        "subvol=root"
-        "compress=zstd"
-      ];
-    };
-    "/home" = {
-      device = "/dev/disk/by-label/nixos";
-      fsType = "btrfs";
-      options = [
-        "subvol=home"
-        "compress=zstd"
-      ];
-    };
-    "/nix" = {
-      device = "/dev/disk/by-label/nixos";
-      fsType = "btrfs";
-      options = [
-        "subvol=nix"
-        "compress=zstd"
-        "noatime"
-      ];
-    };
-    "/boot" = {
-      device = "/dev/disk/by-uuid/2ED7-2A30";
-      fsType = "vfat";
-      options = [ "umask=0077" ];
-    };
-    "/mnt/drive2" = {
-      device = "/dev/disk/by-label/drive2";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/97943bb1-2816-47e7-9d5e-fcffb744cc68";
+    fsType = "btrfs";
+    options = [
+      "subvol=root"
+      "compress=zstd"
+    ];
+  };
+
+  fileSystems."/home" = {
+    device = "/dev/disk/by-uuid/97943bb1-2816-47e7-9d5e-fcffb744cc68";
+    fsType = "btrfs";
+    options = [
+      "subvol=home"
+      "compress=zstd"
+    ];
+  };
+  fileSystems."/nix" = {
+    device = "/dev/disk/by-uuid/97943bb1-2816-47e7-9d5e-fcffb744cc68";
+    fsType = "btrfs";
+    options = [
+      "subvol=nix"
+      "compress=zstd"
+      "noatime"
+    ];
+  };
+
+  fileSystems."/mnt/d1" = {
+    device = "/dev/disk/by-uuid/28cea1cd-4796-4299-aace-66c39b29ebb0";
+    fsType = "btrfs";
+    options = [
+      "compress=zstd"
+    ];
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/CC67-63AE";
+    fsType = "vfat";
+    options = [
+      "fmask=0077"
+      "dmask=0077"
+    ];
   };
 
   swapDevices = [
-    { device = "/dev/disk/by-uuid/f2cb2ead-1cb7-46f3-b7b0-e4e84fa49025"; }
+    { device = "/dev/disk/by-uuid/412ee7db-6713-4016-807c-5c4cfee222c6"; }
   ];
   zramSwap.enable = true;
 
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [
+      libva-vdpau-driver # required by davinci resolve
+
+      intel-media-driver # vaapi
+      vpl-gpu-rt # vpl api
+      intel-compute-runtime # opencl
+    ];
+    extraPackages32 = with pkgs.pkgsi686Linux; [
+      intel-media-driver
+    ];
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    open = true;
+    modesetting.enable = true;
+    nvidiaSettings = true;
+  };
 
   services.fstrim.enable = true; # for ssd
+  services.thermald.enable = true;
+
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
