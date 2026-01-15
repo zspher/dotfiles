71f13efa91a2d50d3569c35cf91c4988662267a7
zspher <66728045+zspher@users.noreply.github.com>
2026-01-15 00:22:18 -0700

    fix(dolphin): open with on non-Kde DEs

--- home/programs/utils/dolphin.nix
+++ home/programs/utils/dolphin.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   home.packages = with pkgs.kdePackages; [
     dolphin
@@ -10,4 +10,17 @@
     qtimageformats # for webp thumbnails
   ];
   qt.kde.settings."baloofilerc"."Basic Settings"."Indexing-Enabled" = false;
+
+  # to fix `open with` in non-kde DEs
+  home.activation.fixOpenWith = config.lib.dag.entryAfter [ "writeBoundary" ] ''
+    menu_dir=${config.xdg.configHome}/menus
+    if [[ ! -e "$menu_dir/applications.menu" ]]; then
+      mkdir -p "$menu_dir"
+      cp -f "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu" \
+        "$menu_dir/applications.menu"
+
+      rm ${config.xdg.cacheHome}/ksycoca6_*
+      run ${pkgs.kdePackages.kservice}/bin/kbuildsycoca6
+    fi
+  '';
 }
