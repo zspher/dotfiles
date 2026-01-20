d17f27e22729075368e688a1abe269831910c2a7
zspher <66728045+zspher@users.noreply.github.com>
2026-01-19 20:38:01 -0700

    feat(hyprlock): remove depreciated options

--- home/programs/wayland/hyprlock.nix
+++ home/programs/wayland/hyprlock.nix
@@ -18,9 +18,7 @@ in
     enable = true;
     settings = {
       general = {
-        disable_loading_bar = true;
         hide_cursor = true;
-        no_fade_in = true;
         ignore_empty_input = false;
       };
 
