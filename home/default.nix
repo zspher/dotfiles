6a31f9cfa01481568b6c4c8689c974e3e919353f
zspher <66728045+zspher@users.noreply.github.com>
2026-03-04 17:23:31 -0700

    feat: remove home-manager lix fix with update

--- home/default.nix
+++ home/default.nix
@@ -31,14 +31,4 @@
       "[ ! -e ${p} ] && mkdir -p $(dirname ${x}) && touch ${p}\n"
     ) (builtins.attrNames config.qt.kde.settings)}
   '';
-
-  # FIX: home-manager broken on lix https://github.com/nix-community/home-manager/issues/8786
-  home.activation.installPackages = lib.mkForce (lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    nixProfileRemove home-manager-path
-    if [[ -e ${config.home.profileDirectory}/manifest.json ]]; then
-      run nix profile install ${config.home.path}
-    else
-      run nix-env -i ${config.home.path}
-    fi
-  '');
 }
