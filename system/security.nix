dd5a80386f9df8766fc9685faf810228240bbcee
zspher <66728045+zspher@users.noreply.github.com>
2026-03-18 23:28:56 -0600

    feat: disable sudo-rs for now

--- system/security.nix
+++ system/security.nix
@@ -10,7 +10,12 @@
     polkit.enable = true;
     # useful for pipewire
     rtkit.enable = true;
-    sudo-rs.enable = true;
+
+    # NOTE: sudo-rs breaks certain apps like
+    #       - KDE partitionmanager (when using external ssd)
+    #       so use default sudo for now
+    #       probably related to `-E`
+    # sudo-rs.enable = true;
 
     pam.services.hyprlock = { };
   };
