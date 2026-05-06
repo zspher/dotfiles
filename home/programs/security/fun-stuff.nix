309858ebcdf600a575f738a9bd225dfd202be99a
zspher <66728045+zspher@users.noreply.github.com>
2026-05-05 18:31:32 -0600

    fix: disable cutter; build failure

--- home/programs/security/fun-stuff.nix
+++ home/programs/security/fun-stuff.nix
@@ -9,13 +9,14 @@
     wireshark
 
     # reverse engineering
-    (cutter.withPlugins (
-      ps: with ps; [
-        jsdec
-        rz-ghidra
-        sigdb
-      ]
-    ))
+    # NOTE: disabled due to build failure. will be fixed in next release
+    # (cutter.withPlugins (
+    #   ps: with ps; [
+    #     jsdec
+    #     rz-ghidra
+    #     sigdb
+    #   ]
+    # ))
 
     # osint tools
     self.packages.${pkgs.stdenv.hostPlatform.system}.barcode-reader-cli
