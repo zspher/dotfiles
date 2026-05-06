5062d94fda8423968a84f6fced72d531f36d2b6e
zspher <66728045+zspher@users.noreply.github.com>
2026-05-05 18:32:14 -0600

    feat(games): add lutris

--- home/programs/games/default.nix
+++ home/programs/games/default.nix
@@ -5,9 +5,16 @@
   ];
   home.packages = with pkgs; [
     # emulators / compatibility layer
-    (bottles.override { removeWarningPopup = true; })
+    # (bottles.override { removeWarningPopup = true; })
     cemu
 
     inputs.fjordLauncher.packages.${pkgs.stdenv.hostPlatform.system}.fjordlauncher
   ];
+  programs.lutris = {
+    enable = true;
+    runners = {
+      cemu.package = pkgs.cemu;
+      pcsx2.package = pkgs.pcsx2;
+    };
+  };
 }
