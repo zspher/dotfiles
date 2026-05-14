b7f802bf2814e2b6debf046df4789bf20b3b204e
zspher <66728045+zspher@users.noreply.github.com>
2026-05-14 11:30:19 -0600

    Revert "fix: disable cutter; build failure"
    This reverts commit 309858ebcdf600a575f738a9bd225dfd202be99a.

--- home/programs/security/fun-stuff.nix
+++ home/programs/security/fun-stuff.nix
@@ -9,14 +9,13 @@
     wireshark
 
     # reverse engineering
-    # NOTE: disabled due to build failure. will be fixed in next release
-    # (cutter.withPlugins (
-    #   ps: with ps; [
-    #     jsdec
-    #     rz-ghidra
-    #     sigdb
-    #   ]
-    # ))
+    (cutter.withPlugins (
+      ps: with ps; [
+        jsdec
+        rz-ghidra
+        sigdb
+      ]
+    ))
 
     # osint tools
     self.packages.${pkgs.stdenv.hostPlatform.system}.barcode-reader-cli
