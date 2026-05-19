3a3fb3ab5654f6cc04fab2da1945bd3f8feeee9c
zspher <66728045+zspher@users.noreply.github.com>
2026-05-19 14:46:15 -0600

    feat(hyprland): use wayland.windowManager.hyprland.settings for apps

--- home/programs/wayland/hyprland/default.nix
+++ home/programs/wayland/hyprland/default.nix
@@ -1,4 +1,9 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 {
   imports = [
     ./keybinds.nix
@@ -7,15 +12,22 @@
   xdg.configFile = {
     "hypr/hyprland.lua".source = ./hyprland.lua;
     "hypr/catppuccin.lua".source = ./catppuccin.lua;
-    "hypr/apps.lua".text = ''
-      local M = {
-        clipboard_manager = "vicinae 'vicinae://launch/clipboard/history'",
-        power_menu = "vicinae 'vicinae://launch/power'",
-        runner = "vicinae toggle",
-        screenshot = "vicinae 'vicinae://launch/@zspher/screenshot/screenshot'",
-      }
-      return M
-    '';
+    "hypr/apps.lua".text =
+      let
+        attrs = lib.filterAttrs (
+          k: v: builtins.substring 0 1 k == "$"
+        ) config.wayland.windowManager.hyprland.settings;
+
+        data = builtins.concatStringsSep "\n  " (
+          lib.mapAttrsToList (k: v: "${builtins.replaceStrings [ "$" ] [ "" ] k} = \"${v}\",") attrs
+        );
+      in
+      ''
+        local M = {
+          ${data}
+        }
+        return M
+      '';
     "hypr/.luarc.json" = {
       text = builtins.toJSON {
         workspace.library = [ "${pkgs.hyprland}/share/hypr/stubs" ];
