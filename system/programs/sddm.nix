0d1ae2ee085fdcc78e5c1f18b4c925f12e535d9b
zspher <66728045+zspher@users.noreply.github.com>
2026-06-28 22:34:12 -0600

    fix(sddm): invisible mouse issue
    - no mouse on weston

--- system/programs/sddm.nix
+++ system/programs/sddm.nix
@@ -9,12 +9,18 @@
     enable = true;
     theme = "sddm-corners";
     wayland.enable = true;
+    wayland.compositor = "kwin";
+    settings.Theme = {
+      CursorTheme = "Posy_Cursor";
+      CursorSize = 24;
+    };
   };
 
   environment.systemPackages = [
     (self.packages.${pkgs.stdenv.hostPlatform.system}.sddm-corners-theme.override {
       font = "CaskaydiaMono Nerd Font";
     })
+    pkgs.posy-cursors
   ];
 
   systemd.user.tmpfiles.users.${username}.rules = [
