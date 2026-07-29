7c813eeba7417cd5405cab46ac77d080b9749f8f
zspher <66728045+zspher@users.noreply.github.com>
2026-07-29 17:34:30 -0600

    feat(vicinae): update config
    - add some extensions
    - disable `browser` & input_server

--- home/programs/wayland/vicinae.nix
+++ home/programs/wayland/vicinae.nix
@@ -1,11 +1,33 @@
 { pkgs, config, ... }:
+let
+  vicinaeExtensions =
+    pkgs.fetchFromGitHub {
+      owner = "vicinaehq";
+      repo = "extensions";
+      rev = "3ac7d25baa16ad5f1a6227c3e4b4b21a7c3e452b";
+      sha256 = "sha256-7kyqXXQyRVy41vWled33nJZFbtrGaBJlO4+uSE0Ct18=";
+    }
+    + "/extensions";
+
+in
 {
   programs.vicinae = {
     enable = true;
     systemd.enable = true;
     extensions = [
       (config.lib.vicinae.mkExtension {
+        name = "color-converter";
+        npmDepsHash = "sha256-EYvMY+NoCm8NaU3pEV5QUYEUkYZZiYXx7eCDD20TzrM=";
+        src = vicinaeExtensions + "/color-converter";
+      })
+      (config.lib.vicinae.mkExtension {
+        name = "nix";
+        npmDepsHash = "sha256-TEyCCDjAtRYX2uH2TpLfe4/hTzyfMiyDhzVdyQXhEus=";
+        src = vicinaeExtensions + "/nix";
+      })
+      (config.lib.vicinae.mkExtension {
         name = "screenshot";
+        npmDepsHash = "sha256-6Ou1bkdCpg89iCBZRGVUQH/mBIxdRvlQH8HM+TXbyZo=";
         src =
           pkgs.fetchFromGitHub {
             owner = "zspher";
@@ -20,7 +42,9 @@
     settings = {
       fallbacks = [ ];
       telemetry.system_info = false;
+      input_server.enabled = false;
       providers = {
+        "browser-extension".enabled = false;
         files.enabled = false;
         files.preferences.autoIndexing = false;
         snippets.enabled = false;
