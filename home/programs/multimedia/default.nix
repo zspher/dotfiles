8bf76b925979377f74c326da74916e828cc10921
zspher <66728045+zspher@users.noreply.github.com>
2026-09-03 19:03:43 -0600

    Revert "feat(vicinae): use vscrot screenshot extension"
    This reverts commit 514af633dd9ac4c3c849e53a134c4cf093518b91.

--- home/programs/multimedia/default.nix
+++ home/programs/multimedia/default.nix
@@ -12,7 +12,6 @@
     hyprpicker
     inkscape
     qimgv
-    satty
     timg
   ];
 }
--- home/programs/wayland/vicinae.nix
+++ home/programs/wayland/vicinae.nix
@@ -4,8 +4,8 @@ let
     pkgs.fetchFromGitHub {
       owner = "vicinaehq";
       repo = "extensions";
-      rev = "de926d2e94ff4423dc04068eb2b6fc8d501f3b74";
-      sha256 = "sha256-YVr/SysyzG2lkSpacrEC/Mo7XdUYdKu5vX2AUcqAzpQ=";
+      rev = "3ac7d25baa16ad5f1a6227c3e4b4b21a7c3e452b";
+      sha256 = "sha256-7kyqXXQyRVy41vWled33nJZFbtrGaBJlO4+uSE0Ct18=";
     }
     + "/extensions";
 
@@ -25,15 +25,17 @@ in
         npmDepsHash = "sha256-TEyCCDjAtRYX2uH2TpLfe4/hTzyfMiyDhzVdyQXhEus=";
         src = vicinaeExtensions + "/nix";
       })
-      # (config.lib.vicinae.mkExtension { # does not work
-      #   name = "dbus";
-      #   npmDepsHash = "sha256-FcSA72bmQVWY7i+iAqysUkfTH4FuQAEVAgG8o7lfQf0=";
-      #   src = vicinaeExtensions + "/dbus";
-      # })
       (config.lib.vicinae.mkExtension {
-        name = "vscrot"; # screenshot
-        npmDepsHash = "sha256-+x66mx89+hnBtvYptg+IGiAT+KqoJOgQYhzXhr6nS58=";
-        src = vicinaeExtensions + "/vscrot";
+        name = "screenshot";
+        npmDepsHash = "sha256-6Ou1bkdCpg89iCBZRGVUQH/mBIxdRvlQH8HM+TXbyZo=";
+        src =
+          pkgs.fetchFromGitHub {
+            owner = "zspher";
+            repo = "vicinae_extensions";
+            rev = "748ce4a2174f51c224c30a521d60b0834020d8f8";
+            hash = "sha256-tNGpEqEQ20cfdg8plQtw5FR4QEMRFWXsHZY1J/B82oA=";
+          }
+          + "/screenshot";
       })
       # TODO: add websearch extension
     ];
@@ -48,8 +50,6 @@ in
         snippets.enabled = false;
         clipboard.entrypoints.history.preferences.defaultAction = "copy";
         power.entrypoints.logout.preferences.customProgram = "uwsm stop";
-
-        "@JaINTP/vscrot".entrypoints.scrot.alias = "screenshot";
       };
     };
   };
@@ -58,7 +58,7 @@ in
     "$runner" = "vicinae toggle";
     "$clipboard_manager" = "vicinae 'vicinae://launch/clipboard/history'";
     "$power_menu" = "vicinae 'vicinae://launch/power'";
-    "$screenshot" = "vicinae 'vicinae://launch/@JaINTP/vscrot/scrot'";
+    "$screenshot" = "vicinae 'vicinae://launch/@zspher/screenshot/screenshot'";
   };
 
   catppuccin.vicinae.enable = true;
